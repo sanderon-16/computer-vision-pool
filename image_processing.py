67f0e209dff7cde373c785874201edac8b8c6330
Ron Sendrovich <ron.sendrovich.44@gmail.com>
2024-02-07 10:18:51 +0200

    made a function that transforms image into a rect

--- image_processing.py
+++ image_processing.py
@@ -40,6 +40,29 @@ def transform_board(image: Image, rect) -> Image:
 
     return transformed_image
 
+def generate_projection(image_to_project: Image, rect) -> Image:
+    x1, y1, x2, y2, x3, y3, x4, y4 = rect
+
+    # Define the target rectangle dimensions
+    target_width = 1920
+    target_height = 1080
+
+    # Define the source rectangle (coordinates of the four corners)
+    src_pts = np.float32(
+        [[0, 0], [image_to_project.shape[1], 0], [image_to_project.shape[1], image_to_project.shape[0]],
+         [0, image_to_project.shape[0]]])
+
+    # Define the target rectangle (coordinates of the four corners)
+    target_pts = np.float32([[0, 0], [target_width, 0], [target_width, target_height], [0, target_height]])
+
+    # Compute the perspective transformation matrix
+    matrix = cv2.getPerspectiveTransform(src_pts, np.float32([[x1, y1], [x2, y2], [x3, y3], [x4, y4]]))
+
+    # Apply the perspective transformation to the image
+    projected_image = cv2.warpPerspective(image_to_project, matrix, (target_width, target_height),
+                                          borderValue=(0, 0, 0))
+
+    return projected_image
 
 def find_balls(image: Image) -> List[Ball]:
     """
@@ -68,12 +91,12 @@ def show_image(image: Image):
 
 def main(image: Image):
     rect = [817, 324, 1186, 329, 1364, 836, 709, 831]
-    board = transform_board(image, rect)
+    board = generate_projection(image, rect)
     show_image(board)
-    cv2.imwrite('cropped_board.png', board)
+    cv2.imwrite('proj.png', board)
 
 
 if __name__ == "__main__":
-    filepath = r"C:\Users\TLP-299\PycharmProjects\computer-vision-pool\downloaded_images\board_uncropped_with_stick.jpg"
+    filepath = r"C:\Users\TLP-299\PycharmProjects\computer-vision-pool\output_images\cropped_board.png"
     image = cv2.imread(filepath)
     main(image)
